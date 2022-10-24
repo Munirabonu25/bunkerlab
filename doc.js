2bd434efd35d84b800d6554eaae3a65387a84668
Munirabonu <muniratillaxojayeva@gmail.com>
2022-10-24 15:03:41 +0500

    html_savollar

---
+++ doc.js
@@ -0,0 +1,17 @@
+// html (main, footer, header ...) teglari nima uchun kerak
+// position: fixed; va position: relative;
+// qanday ishni bajaradi?
+
+// inner-block divini qanday qilib centerga keltirish mumkin
+// ( flex, grid, margin, POSITION )
+
+// . Block va inline elementlari orasida nima farq bor?
+// . HTML Semantic elementlari nima?
+// . Accessibility nima?
+
+// Html semantik teglar nima uchun keraklig
+// Div span teglarini farqi
+//  Kontentni sahifani oʻrtasiga qanday qilib joylanadi
+//  Flexbox haqida
+// - Box model haqida soʻraldi.
+// - position relative, static, fixed, sticky ni farqi
